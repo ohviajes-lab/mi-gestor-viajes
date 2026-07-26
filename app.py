674516a9c514197e5a5aecabb82ecfe862d001ef
ohviajes-lab <ohviajes@gmail.com>
2026-07-26 18:53:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ if menu == "🧮 Crear Presupuesto":
                     contents.append(prompt)
 
                     response = client.models.generate_content(
-                        model="gemini-2.0-flash",
+                        model="gemini-1.5-flash",
                         contents=contents,
                         config=types.GenerateContentConfig(response_mime_type="application/json")
                     )
